16eb74bea3899e87358002b621c55a7958bcbf37
kolomu <kevin-olomu@web.de>
2020-03-01 17:54:57 +0100

    Added datasource.properties file for external properties which are read in the PropertyConfig File which is generating the beans for FakeDataSource bean.

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,40 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+
+@Configuration
+@PropertySource("classpath:datasource.properties") // we want to use this set of properties
+public class PropertyConfig {
+
+    // Spring evaluated expression -> go into spring context and get that value as externalized property!
+    @Value("${mydb.username}")
+    String user;
+
+    @Value("${mydb.password}")
+    String password;
+
+    @Value("${mydb.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        // this is reading the file for us. -> enable us to use @Value annotation.
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
